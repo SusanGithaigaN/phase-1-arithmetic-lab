a0ff930c70bd300492c8f8c4a8910564a6259889
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-23 11:09:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// Create new variable.
+const num1= 31;
+const num2= 2;
+const multiply = num1 * num2;
+//Generate random number greater than 0.
+const random = Math.floor(Math.random() * 10) +1;
 
-// Write your code here
+const num3 = 14;
+const num4 = 5;
+const mod = 14 % 5;
+
+const max = Math.max(5,10,15,20);
